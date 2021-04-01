56de9634fbe9e7b27f0f546b2d943cfb4c1d4218
Liam <doyll553@newschool.edu>
2021-03-31 22:10:31 -0400

    one

---
+++ main.js
@@ -0,0 +1,126 @@
+console.log("airtable demo");
+
+var Airtable = require('airtable');
+console.log("Airtable");
+var base = new Airtable({apiKey: 'keyDvpfoibL19TD8A'}).base('app68AKsQa0Jgl05p');
+
+base('sneakers').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('sneaker_name'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('image'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('retail_price'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('current_resell_price'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('marketed_to:'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('release_date'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('drop_count'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('style'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('color_way'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('description'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('volatility'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('designer'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+
